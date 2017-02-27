004908c473ba171fd5b46183f293e9a8bcf65c83
Valdriano Oliveira <valdriano.oliveira@gmail.com>
2017-02-27 09:33:50 -0400

    Projeto Open Source
    Gostaria de ajuda pra desenvolver  esse projeto onde eu possar fazer o
    controle de atualização da minha base de dados.

---
+++ ValSystem/ValSystem.Connection/Connections.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace ValSystem.Connection
+{
+    public class Connections : IConnections
+    {
+        public OleDbCommand Cmd( OleDbConnection conn, string cmd, params OleDbParameter[] paramentos )
+        {
+            OleDbCommand Command = new OleDbCommand();
+
+            try
+            {
+                return Command;
+            }
+            catch ( Exception ex )
+            {
+
+                throw new Exception( ex.Message );
+            }
+            finally
+            {
+                Command.Dispose();
+            }
+        }
+
+        public OleDbConnection OpenConn()
+        {
+            OleDbConnection conn = new OleDbConnection();
+
+            try
+            {
+                conn.ConnectionString = $"";
+
+                return conn;
+            }
+            catch ( Exception ex )
+            {
+
+                throw new Exception( ex.Message );
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+        }
+    }
+}
---
+++ ValSystem/ValSystem.Connection/IConnections.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace ValSystem.Connection
+{
+    interface IConnections
+    {
+        OleDbConnection OpenConn();
+        OleDbCommand Cmd( OleDbConnection conn, string cmd, params OleDbParameter[] paramentos );
+
+    }
+}
